18b848b628fa95e813a135b7c3e4e0f3a16a23bb
Henryque <henryque_1@hotmail.com>
2020-11-08 21:13:26 -0300

    updated files -- 08/11/20

--- instructors.js
+++ instructors.js
@@ -1,6 +1,29 @@
 const fs = require('fs');
 const data = require('./data.json');
 
+//show
+exports.show = (req, res) => {
+    const { id } = req.params;
+
+    const foundInstructor = data.instructors.find((instructor) => {
+        return instructor.id == id;
+    });
+
+    if(!foundInstructor) {
+        return res.send("Instructor not found!")
+    }
+
+    const instructor = {
+        ...foundInstructor,
+        age: "",
+        services: foundInstructor.services.split(","),
+        created_at: ""
+    }
+
+    return res.render("instructors/show", { instructor });
+    //return res.send(foundInstructor);
+}
+
 // create
 exports.post = (req, res) => {
 
