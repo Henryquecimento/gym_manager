b4a3b7c8355febece690ab2f22267ac6f9de8d43
Henryque <henryque_1@hotmail.com>
2020-11-09 23:28:20 -0300

    updated files --DATE.js

---
+++ date.js
@@ -0,0 +1,21 @@
+module.exports = {
+    age: (timestamp) => {
+        const today = new Date();
+        const birthDate = new Date(timestamp);
+    
+        //2019 - 1999
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const month = today.getMonth() - birthDate.getMonth();
+    
+        
+         
+        // 11 - 12 = -1
+        // 11 - 11 = 0
+        if(month < 0 || month == 0 && today.getDate() < birthDate.getDate()) {
+            age = age - 1;
+        }
+    
+        return age;
+    }
+}
+
